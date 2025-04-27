790f6fd73561b1a474e748f874a0c1c8cc80cd38
Ahmad Saleh <dev.ahmadsaleh@gmail.com>
2025-04-27 07:01:34 +0300

    fixing the backoff, arch of docker image, and order of versions

--- terraform/modules/secret_manager/main.tf
+++ terraform/modules/secret_manager/main.tf
@@ -3,20 +3,22 @@ resource "google_secret_manager_secret" "secret" {
   labels = {
     used_in = "demo-service"
   }
-
   replication {
     auto {}
   }
 }
 
-# Securely fetch the secret from an external source
+resource "google_secret_manager_secret_version" "secret-version-basic" {
+  secret         = google_secret_manager_secret.secret.id
+  secret_data_wo = file(var.secret_file_path)
+  depends_on     = [google_secret_manager_secret.secret]
+}
+
 data "google_secret_manager_secret_version" "secret-latest" {
   secret  = google_secret_manager_secret.secret.id
   version = "latest"
-}
 
-resource "google_secret_manager_secret_version" "secret-version-basic" {
-  secret          = google_secret_manager_secret.secret.id
-  secret_data_wo  = file(var.secret_file_path) # Securely reading from an external file
-  depends_on      = [google_secret_manager_secret.secret]
+  depends_on = [
+    google_secret_manager_secret_version.secret-version-basic
+  ]
 }
